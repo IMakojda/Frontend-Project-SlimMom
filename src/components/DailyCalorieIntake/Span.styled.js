29a200615590973272e69600ba63feac003b689d
sergiosergio2 <vzdor5555@gmail.com>
2022-07-12 10:52:47 +0300

    add style

---
+++ src/components/DailyCalorieIntake/Span.styled.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+const Span = styled.p`
+
+
+font-family: 'Verdana';
+font-style: normal;
+font-weight: 400;
+font-size: 14px;
+line-height: 17px;
+letter-spacing: 0.04em;
+
+color: #9B9FAA;
+`
+
+
+export default Span;

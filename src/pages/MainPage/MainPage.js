fac9ef537321e4003b9e52d953540539603ec845
bohdana <halenkobohdana@gmail.com>
2022-07-15 13:28:25 +0300

    modal

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,11 +1,26 @@
-
+import { useState } from 'react';
 import Container from '../../components/Container';
 import CalculatorFormWrapper from '../../components/Forms/CalculatorFormWrapper';
+import { Modal } from '../../components/Modal';
+import { ModalContent } from '../../components/Modal/ModalContent';
 
 export default function MainPage() {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => {
+    setShowModal(prev => !prev);
+  };
+  if (showModal) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'unset';
+  }
+
   return (
     <Container>
-      <CalculatorFormWrapper />
+      <CalculatorFormWrapper openModal={openModal} />
+      <Modal showModal={showModal} setShowModal={setShowModal}>
+        <ModalContent setShowModal={setShowModal} />
+      </Modal>
     </Container>
   );
 }

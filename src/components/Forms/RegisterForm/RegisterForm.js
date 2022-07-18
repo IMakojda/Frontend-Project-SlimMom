5ac8661b7f4ccf06a1b0ca7c4c7b4a29402240b0
Peleshko <82321691+Peleshko@users.noreply.github.com>
2022-07-18 18:45:48 +0300

    add little corrections

--- src/components/Forms/RegisterForm/RegisterForm.js
+++ src/components/Forms/RegisterForm/RegisterForm.js
@@ -122,17 +122,17 @@ export default function RegisterForm() {
               <PasswordEye onClick={handleClick}>
                 {eyeOutlined ? (
                   <img
-                    src={visibility}
-                    width="24px"
-                    height="24px"
-                    alt="visibility"
-                  />
-                ) : (
-                  <img
                     src={visibility_off}
                     width="24px"
                     height="24px"
                     alt="visibility_off"
+                  />
+                ) : (
+                  <img
+                    src={visibility}
+                    width="24px"
+                    height="24px"
+                    alt="visibility"
                   />
                 )}
               </PasswordEye>
--- src/pages/RegistrationPage/RegistrationPage.js
+++ src/pages/RegistrationPage/RegistrationPage.js
@@ -1,28 +1,13 @@
 import RegisterForm from 'components/Forms/RegisterForm/RegisterForm';
-import styled from 'styled-components';
 import { motion } from 'framer-motion';
-const RegisterWrapper = styled.h1`
-  margin-top: 40px;
-  font-family: 'Verdana';
-  font-size: 14px;
-  line-height: 17px;
-  text-align: center;
-  letter-spacing: 0.04em;
-  text-transform: uppercase;
-  color: #fc842d;
-  @media screen and (min-device-width: 768px) {
-    margin-top: 160px;
-    margin-left: 32px;
-    text-align: start;
-  }
-`;
+import { RegisterWrapper } from './RegistrationPage.styled';
 
 export default function RegistrationPage() {
   return (
     <motion.div
-      initial={{opacity:0}}
-      animate={{opacity:1}}
-      exit={{opacity:0, transition:{duration:0.3}}}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, transition: { duration: 0.3 } }}
     >
       <RegisterWrapper>реєстрація</RegisterWrapper>
       <RegisterForm />

4e98fea77f45492bfe3e7216a5e0daa75d999d20
IMakojda <86243683+IMakojda@users.noreply.github.com>
2022-07-20 15:55:40 +0000

    Deploying to gh-pages from @ IMakojda/Frontend-Project-SlimMom@61b4de39bb7e04868f6caddb7457fa613d02bbb6 🚀

---
+++ static/js/697.3dc54f9c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend_project_slimmom=self.webpackChunkfrontend_project_slimmom||[]).push([[697],{3697:function(t,n,i){i.r(n),i.d(n,{default:function(){return r}});var e=i(441),o=i(184);function r(){return(0,o.jsx)(e.E.div,{initial:{opacity:0},animate:{opacity:1},exit:{opacity:0,transition:{duration:.3}},children:(0,o.jsxs)("h1",{children:["404 Page not found ",(0,o.jsx)("br",{}),":(  Please go to Contact "," "]})})}}}]);
+//# sourceMappingURL=697.3dc54f9c.chunk.js.map
--- static/js/697.bcaa4a00.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfrontend_project_slimmom=self.webpackChunkfrontend_project_slimmom||[]).push([[697],{3697:function(t,n,i){i.r(n),i.d(n,{default:function(){return r}});var e=i(1029),o=i(184);function r(){return(0,o.jsx)(e.E.div,{initial:{opacity:0},animate:{opacity:1},exit:{opacity:0,transition:{duration:.3}},children:(0,o.jsxs)("h1",{children:["404 Page not found ",(0,o.jsx)("br",{}),":(  Please go to Contact "," "]})})}}}]);
-//# sourceMappingURL=697.bcaa4a00.chunk.js.map

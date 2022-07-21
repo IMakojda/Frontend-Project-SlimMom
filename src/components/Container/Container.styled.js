b0a5de2aaf5b297f8753efe9b7a84ad52beb34e8
oleksandr-suslov <76252100+oleksandr-suslov@users.noreply.github.com>
2022-07-21 18:50:27 +0300

    fix background

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -5,14 +5,14 @@ export const ResponsiveContainer = styled.div`
   margin-right: auto;
   padding: 20px 20px;
   width: 100%;
-  height: 1000px;
+  /* height: 1000px; */
 
   @media screen and (min-width: 768px) and (max-width: 1279px) {
     padding: 20px 20px;
   }
 
   @media screen and (min-width: 1280px) {
-     height: 850px;
+     /* height: 850px; */
      width: 1280px;
   }
 `;

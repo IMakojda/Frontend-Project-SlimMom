2a5bb558d20dad0cd7057274eac8ffce81bf8e52
IMakojda <86243683+IMakojda@users.noreply.github.com>
2022-07-23 09:34:49 +0000

    Deploying to gh-pages from @ IMakojda/Frontend-Project-SlimMom@917a7fb5160b3594a4ce3e6be8d29d9802ff6418 🚀

---
+++ static/js/697.37e149f7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend_project_slimmom=self.webpackChunkfrontend_project_slimmom||[]).push([[697],{3697:function(n,t,i){i.r(t),i.d(t,{default:function(){return r}});var e=i(441),o=i(184);function r(){return(0,o.jsx)(e.E.div,{initial:{opacity:0},animate:{opacity:1},exit:{opacity:0,transition:{duration:.3}},children:(0,o.jsxs)("h1",{children:["404 Page not found ",(0,o.jsx)("br",{}),":(  Please login  "," "]})})}}}]);
+//# sourceMappingURL=697.37e149f7.chunk.js.map
